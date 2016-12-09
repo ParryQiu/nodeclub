edab6ae4b45cc5c0d74dcefb5749278393529f55
ParryQiu <qiupengyuan@gmail.com>
2016-12-09 23:22:33 +0800

    更新 getstart 的标题。

--- controllers/static.js
+++ controllers/static.js
@@ -14,7 +14,7 @@ exports.faq = function (req, res, next) {
 
 exports.getstart = function (req, res) {
   res.render('static/getstart', {
-    pageTitle: 'Node.js 新手入门'
+    pageTitle: '微信小程序新手入门资源'
   });
 };
 
